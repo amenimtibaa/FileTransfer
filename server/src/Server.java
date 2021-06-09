4f1e53e11c28d40a4927fa90d4fe8cff05acdb41
amenimtibaa <ameni.mtibaa17@gmail.com>
2021-06-10 01:53:45 +0200

    adapt server to support multiple actions

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,4 +1,3 @@
-// A Java program for a Server
 import java.net.*;
 
 import custom.libs.FilesTransfer;
@@ -9,12 +8,15 @@ public class Server
 {
     //initialize socket and input stream
 	public final static int FILE_SIZE = 6022386;
-	public final static String FILE_TO_RECEIVED = "2.jpg"; //"graduate.docx";
-	
+	public final static String FILE_TO_RECEIVED = "test.txt"; //"2.jpg"; //"graduate.docx";
+	public final static String FILE_TO_SEND = "test.txt";
 	private ServerSocket server;
     private Socket socket;
-    private DataInputStream in;
+    private BufferedReader in;
   
+    /**
+     * @param port
+     * */
     public Server(int port)
     {
         
@@ -26,22 +28,37 @@ public class Server
   
             socket = server.accept();
             System.out.println("Client accepted"); 
-            FilesTransfer.saveFile(socket, FILE_TO_RECEIVED,  FILE_SIZE);
-            System.out.println("Done saving !");
             
-            System.out.println("Closing connection");
-            
-            // close connection
-            socket.close();
-
+            in = new BufferedReader(new InputStreamReader(
+            		socket.getInputStream()));
+            String clientSelection = in.readLine();
+            while(true) 
+            {        
+	            switch (clientSelection) {
+	            	case "1":
+	            		FilesTransfer.saveFile(socket, FILE_TO_RECEIVED,  FILE_SIZE);
+	            		System.out.println("Done saving !");
+	            		continue;
+	            	case "2":
+	            		FilesTransfer.sendFile(FILE_TO_SEND, socket);
+	            		System.out.println("File sent with success !");
+	            		continue;
+	            	case "3":
+	            		System.out.println("Closing connection");
+	            		socket.close();
+	            		break;
+                    default:
+                        System.out.println("Incorrect command received.");
+                        break;
+	            }
+            }
         }
         catch(IOException i)
         {
             System.out.println(i);
         }
-    }
-    
-   
+        
+    }   
     	
     public static void main(String args[])
     {
